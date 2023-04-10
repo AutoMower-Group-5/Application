edb974b0ead8625b86e86a895096dc08ccffb615
neguehard <aurelien.enguehard@epitech.eu>
2023-04-10 22:41:40 +0200

    buildin app first requirements
    push first app test with login + home page

---
+++ MyApplication2/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.myapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var emailEditText: EditText
+    private lateinit var passwordEditText: EditText
+    private lateinit var loginButton: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        emailEditText = findViewById(R.id.emailEditText)
+        passwordEditText = findViewById(R.id.passwordEditText)
+        loginButton = findViewById(R.id.loginButton)
+
+        loginButton.setOnClickListener {
+            val email = emailEditText.text.toString().trim()
+            val password = passwordEditText.text.toString().trim()
+
+            // Here you can add your custom authentication logic
+            // For example, you can check if the email and password match the user's credentials in your database or API
+
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+}
