323f000a83685b6f80c1f21833f1ace0d1b2b56c
tedkief <teddy.kieffer@epitech.eu>
2023-05-03 14:49:46 +0200

    [ADD] Get image from the backend and show them

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -20,16 +20,11 @@ import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
-
-    private val TAG = "BluetoothChatFragment"
-    private val REQUEST_ENABLE_BT = 1
     private lateinit var binding: ActivityMainBinding
 
-    private val mConnectedDeviceName: String? = null
-    private var mConversationArrayAdapter: ArrayAdapter<String>? = null
     private var mOutStringBuffer: StringBuffer? = null
     private var mBluetoothAdapter: BluetoothAdapter? = null
-    private val mService: BluetoothService? = null
+//    private val mService: BluetoothService? = null
 
     @SuppressLint("MissingPermission")
     @RequiresApi(Build.VERSION_CODES.S)
@@ -50,7 +45,7 @@ class MainActivity : AppCompatActivity() {
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
 
-        Permission().checkPermissions(this, applicationContext)
+//        Permission().checkPermissions(this, applicationContext)
     }
 
     @SuppressLint("MissingPermission", "NewApi")
@@ -59,24 +54,24 @@ class MainActivity : AppCompatActivity() {
         if (mBluetoothAdapter == null) {
             return
         }
-        if (!mBluetoothAdapter!!.isEnabled) {
-            val enableIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
-            startActivityForResult(enableIntent, 3)
-        } else if (mService == null) {
-            Permission().checkPermissions(this, applicationContext)
-            mOutStringBuffer = StringBuffer()
-        }
+//        if (!mBluetoothAdapter!!.isEnabled) {
+//            val enableIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+//            startActivityForResult(enableIntent, 3)
+//        } else if (mService == null) {
+//            Permission().checkPermissions(this, applicationContext)
+//            mOutStringBuffer = StringBuffer()
+//        }
     }
 
     @SuppressLint("MissingPermission")
     internal fun ensureDiscoverable() {
-//        if (mBluetoothAdapter!!.scanMode !=
-//            BluetoothAdapter.SCAN_MODE_CONNECTABLE_DISCOVERABLE
-//        ) {
-//            val discoverableIntent = Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE)
-//            discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300)
-//            startActivityForResult(discoverableIntent, 1)
-//        }
+        if (mBluetoothAdapter!!.scanMode !=
+            BluetoothAdapter.SCAN_MODE_CONNECTABLE_DISCOVERABLE
+        ) {
+            val discoverableIntent = Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE)
+            discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300)
+            startActivityForResult(discoverableIntent, 1)
+        }
         val requestCode = 1;
         val discoverableIntent: Intent =
             Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE).apply {
@@ -85,26 +80,8 @@ class MainActivity : AppCompatActivity() {
         startActivityForResult(discoverableIntent, requestCode)
     }
 
-    internal fun sendMessage(message: String) {
-        // Check that we're actually connected before trying anything
-//        if (mService.getState() !== BluetoothChatService.STATE_CONNECTED) {
-//            return
-//        }
-
-        // Check that there's actually something to send
-        if (message.isNotEmpty()) {
-            // Get the message bytes and tell the BluetoothChatService to write
-            val send = message.toByteArray()
-            mService?.write(send)
-
-            // Reset out string buffer to zero and clear the edit text field
-            mOutStringBuffer!!.setLength(0)
-//            mOutEditText.setText(mOutStringBuffer)
-        }
-    }
-
     override fun onDestroy() {
         super.onDestroy()
-        mService!!.stop()
+//        mService!!.stop()
     }
 }
