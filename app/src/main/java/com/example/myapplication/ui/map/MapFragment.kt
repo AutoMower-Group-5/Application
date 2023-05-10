e511ae43eaa515c2fbcf5bf682d0270ac43b2232
neguehard <aurelien.enguehard@epitech.eu>
2023-05-10 12:25:37 +0200

    modification of the map fragment- no crash

--- app/src/main/java/com/example/myapplication/ui/map/MapFragment.kt
+++ app/src/main/java/com/example/myapplication/ui/map/MapFragment.kt
@@ -7,8 +7,12 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
 import com.example.myapplication.APIMower
 import com.example.myapplication.databinding.FragmentMapBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MapFragment : Fragment() {
 
@@ -26,17 +30,23 @@ class MapFragment : Fragment() {
         // Create an instance of the APIMower class
         val apiMower = APIMower()
 
-        // Call the getArrayPath() function
-        val response = apiMower.getArrayPath()
+        // Call the getArrayPath() function using a coroutine
+        lifecycleScope.launch {
+            val response = withContext(Dispatchers.IO) {
+                apiMower.getArrayPath()
+            }
 
-        // Convert the response to a string and display it in a text view
-        response?.let {
-            val pathData = it.string()
-            val textView: TextView = binding.textMap
-            textView.text = pathData
+            // Convert the response to a string and display it in a text view
+            response?.let {
+                val pathData = it.string()
+                val textView: TextView = binding.textMap
+                textView.text = pathData
+            }
         }
+
         return root
     }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
